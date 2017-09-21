fadb5565b583b1f1a671d98670b5dff4b6154526
MattWhittle <themwproject@yahoo.com>
2017-09-21 14:01:57 -0400

    first commit with SearchApp

---
+++ app/components/SearchApp.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+const SearchApp = React.createClass({
+    render: function () {
+        return (
+            <div>
+            SearchApp
+            </div>
+        )
+    }
+});
+
+module.exports = SearchApp;
