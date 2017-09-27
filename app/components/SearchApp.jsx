4b8c33a4ef6ff4f44a672ff266ad82b9a42e1274
MattWhittle <themwproject@yahoo.com>
2017-09-27 08:56:34 -0400

    add static list

--- app/components/SearchApp.jsx
+++ app/components/SearchApp.jsx
@@ -1,10 +1,35 @@
 const React = require('react');
+const SearchList = require('SearchList');
 
 const SearchApp = React.createClass({
+    getInitialState: function() {
+        return {
+            associates: [
+                {
+                    id: 1,
+                    name: 'Matt'
+                },
+                {
+                    id: 2,
+                    name: 'Danielle'
+                },
+                {
+                    id: 3,
+                    name: 'Jen'
+                },
+                {
+                    id: 4,
+                    name: 'Thom'
+                }
+            ]
+        }
+    },
     render: function () {
+        let {associates} = this.state;
+
         return (
             <div>
-            SearchApp
+                <SearchList associates={associates} />
             </div>
         )
     }
